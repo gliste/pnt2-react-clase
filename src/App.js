f308d81e213b4c9097cf9fcaa13504d08de262c0
Gabriela <gabrielaliste@hotmail.com>
2023-10-13 14:51:45 -0300

    Creacion proy React, componente

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import User from './componets/User/User';
 import './App.css';
 
 function App() {
+  const msg = "Soy Gabriela";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <h1>Hola Mundo: {msg}</h1>
+      <User Nombre="Veronica" />
     </div>
   );
 }
---
+++ src/componets/User/User.js
@@ -0,0 +1,7 @@
+const User = (props) => {
+    return (
+        <div>Nombre: {props.Nombre}</div>
+    );
+};
+
+export default User; 
